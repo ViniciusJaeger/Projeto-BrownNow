d09993804f9907308f6dadbfa1053ef7485d3884
ViniciusJaeger <viniciusjaegerprogramador@gmail.com>
2023-06-29 09:44:33 -0300

    'Alterações produtos'

--- produtos.js
+++ produtos.js
@@ -1,38 +1,97 @@
 
-
 function openPopup(product) {
-    var popup = document.getElementById("popup");
-    var popupImage = document.getElementById("popup-image");
-    var popupName = document.getElementById("popup-name");
-    var popupPrice = document.getElementById("popup-price");
-    var popupGallery = document.getElementById("popup-gallery");
+  var popup = document.getElementById("popup");
+  var popupName = document.getElementById("popup-name");
+  var popupPrice = document.getElementById("popup-price");
+  var popupDescription = document.getElementById("popup-description");
 
-    // Defina as informações detalhadas do produto selecionado
-    if (product === "produto1") {
-      popupImage.src = "produto1.jpg";
-      popupName.textContent = "Produto 1";
-      popupPrice.textContent = "R$ 10,00";
-      popupGallery.innerHTML = `
-        <img src="produto1_1.jpg" alt="Produto 1 - Foto 1">
-        <img src="produto1_2.jpg" alt="Produto 1 - Foto 2">
-        <img src="produto1_3.jpg" alt="Produto 1 - Foto 3">
-      `;
-    } else if (product === "produto2") {
-      popupImage.src = "produto2.jpg";
-      popupName.textContent = "Produto 2";
-      popupPrice.textContent = "R$ 15,00";
-      popupGallery.innerHTML = `
-        <img src="produto2_1.jpg" alt="Produto 2 - Foto 1">
-        <img src="produto2_2.jpg" alt="Produto 2 - Foto 2">
-      `;
-    } else if (product === "produto3") {
-      popupImage.src = "produto3.jpg";
-      popupName.textContent = "Produto 3";
-      popupPrice.textContent = "R$ 20,00";
-      popupGallery.innerHTML = `
-        <img src="produto3_1.jpg" alt="Produto 3 - Foto 1">
-      `;
-    }
+  // Defina as informações detalhadas do produto selecionado
+  if (product === "produto1") {
+    popupName.textContent = "Brownie de Castanha";
+    popupPrice.textContent = "R$ 10,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong> Manteiga sem sal, Chocolate meio-amargo, Ovos, Açúcar mascavo, Farinha de trigo, Sal, Castanhas picadas ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  } else if (product === "produto2") {
+    popupName.textContent = "Brownie Gourmet Trufado";
+    popupPrice.textContent = "R$ 15,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong> Chocolate Meio Amargo NESTLÉ® DOIS FRADES® picado, Manteiga derretida, Açúcar, Ovos, Essência de baunilha, farinha de trigo,Cacau em Pó NESTLÉ® DOIS FRADES®,  sal,  Cobertura Chocolate Branco NESTLÉ® DOIS FRADES®, Chocolate Meio Amargo NESTLÉ® DOIS FRADES®, NESTLÉ®, Creme de Leite, MOÇA® ,Avelã";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  } else if (product === "produto3") {
+    popupName.textContent = "Brownie de Óreo Trufado";
+    popupPrice.textContent = "R$ 20,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Manteiga sem sal, Chocolate em tablete, Ovo, Chocolate em pó, Óreo, Açúcar mascavado, Farinha e  Sal. ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  } else if (product === "produto4") {
+    popupName.textContent = "Brownie de Amendoim Elegante";
+    popupPrice.textContent = "R$ 15,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Chocolate meio amargo, Manteiga, Ovo, Açúcar refinado,  Essência de baunilha, Farinha de trigo , Amendoim torrado sem sal e Amendoim torrado inteiro. ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto5") {
+    popupName.textContent = "Brownie de Amendoim Refinado";
+    popupPrice.textContent = "R$ 25,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Chocolate meio amargo, Manteiga, Ovo, Cacau em pó, Açúcar refinado, Farinha, doce de leite, sal, amendoim ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto6") {
+    popupName.textContent = "Brownie de Amendoim Clássico";
+    popupPrice.textContent = "R$ 20,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong> Ovos, Manteiga, Ovo, Cacau em pó, Açúcar, fermento, sal, amendoim, Chocolate meio amargo ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto7") {
+    popupName.textContent = "Brownie de Maracujá Gourmet";
+    popupPrice.textContent = "R$ 30,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Açúcar, Manteiga, Ovo, Polpa de maracujá, Açúcar, Farinha, Chocolate Branco, sal, Rum, Fermento, Bicarbonato de sódio ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto8") {
+    popupName.textContent = "Brownie de Maracujá Maravilha";
+    popupPrice.textContent = "R$ 20,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Açúcar, Manteiga, Ovo, Polpa de maracujá, Açúcar, Farinha, Chocolate Branco, sal, Rum, Fermento, Chocolate meio amargo, Coco ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto9") {
+    popupName.textContent = "Brownie de Maracujá Fresco";
+    popupPrice.textContent = "R$ 20,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Açúcar, Manteiga, Ovo, Polpa de maracujá, Açúcar, Farinha, Chocolate Branco, sal, Rum, Fermento, Chocolate meio amargo, Leite Condensado, Chantilly ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto10") {
+    popupName.textContent = "Brownie de Frutas Silvestres";
+    popupPrice.textContent = "R$ 20,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Açúcar, Manteiga, Ovo,Frutas Silvestres Açúcar mascavo, Farinha, sal, Fermento, Chocolate meio amargo ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto11") {
+    popupName.textContent = "Brownie de Frutas Silvestres";
+    popupPrice.textContent = "R$ 25,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Ovos, Manteiga, Cacau em pó, Açúcar, fermento, sal, amendoim, Chocolate meio amargo, Frutas Silvestres ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }else if (product === "produto12") {
+    popupName.textContent = "Brownie Red Velvet";
+    popupPrice.textContent = "R$ 30,00";
+    popupPrice.style.marginTop = '10px'
+    popupDescription.innerHTML = "<strong>Ingredientes:</strong>  Farinha de trigo sem fermento, Manteiga, Ovos, Cacau em pó, Açúcar refinado, fermento, sal, amendoim, Corante Vermelho, chocolate em barra ao leite ";
+    popupDescription.style.fontSize = '12px';
+    popupDescription.style.marginTop = '10px';
+  }
+
 
     // Abra o pop-up
     popup.style.visibility = "visible";
@@ -47,16 +106,4 @@ function openPopup(product) {
     popup.style.visibility = "hidden";
     popup.style.opacity = 0;
 
-
-    document.removeEventListener("click", outsideClick);
-
-    function outsideClick(event) {
-        var popup = document.getElementById("popup");
-        var popupContent = document.querySelector(".popup-content");
-    
-        // Verifique se o clique ocorreu fora do pop-up
-        if (!popupContent.contains(event.target)) {
-          closePopup();
-        }
-      }
     }
