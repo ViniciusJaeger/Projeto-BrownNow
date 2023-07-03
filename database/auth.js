88aca552746b6d8b807ef839b318042c7f4ff30f
Brunoo47 <125206481+Brunoo47@users.noreply.github.com>
2023-07-03 09:33:13 -0300

    index, cadastro e login

--- database/auth.js
+++ database/auth.js
@@ -1,10 +1,9 @@
 const { default: mongoose, Schema, model } = require("mongoose");
-const cadastro = new Schema({
+const login = new Schema({
     nome :{type:String},
     email:{type:String},
     data: {type:String},
     senha:{type:String}
-    
 })
-const cadastro_model = new model("cadastro", cadastro)
-module.exports = cadastro_model
+const login_model = new model("auth", login)
+module.exports = login_model
---
+++ index.js
@@ -0,0 +1,67 @@
+const login_model = new model("auth", login)
+module.exports = login_model
+const express = require('express')
+const body_parser = require('body-parser')
+const app = express()
+const port = 3000
+const mongoose = require("mongoose")
+mongoose.connect("mongodb+srv://Bruno:javascript77@cluster0.hsqxcqf.mongodb.net/brownow?retryWrites=true&w=majority")
+const db = mongoose.connection
+db.once("open", ()=>{console.log("conectamos ao mongo")})
+
+const login_model = require("./database/auth")
+const exp = require('constants')
+const path = require('path')
+app.use(express.static(path.join(__dirname, "/public")))
+app.use(body_parser.json())
+app.use(body_parser.urlencoded({extended:true}))
+app.get('/teste', (req, res) => {console.log('teste')
+  res.send('Hello World!')
+})
+app.post ("/cadastro", (req, res)=>{
+    // console.log(req.body); fazer o cadastro
+ })
+
+
+
+
+
+
+
+
+app.post ("/login", async (req, res)=>{
+    console.log(req.body);
+   const resultado = await login_model.findOne({email:req.body.logemail})
+    console.log(resultado)
+    if (req.body.logpass===resultado.senha){
+
+        res.redirect('/home.html');
+        
+    } else {
+        res.send('O login nao deu certo.');
+        // todo 
+    }
+})
+
+
+
+
+app.post ("/cadastro2", async (req, res)=>{
+    console.log(req.body);
+   const result9ado = await login_model.create(req.body);
+   res.redirect("/home.html");
+})
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
